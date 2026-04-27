73414b537a386902705769407da9da66a8b16ca6
BaoDuong2359970 <2359970@bdeb.qc.ca>
2026-04-27 01:28:02 -0400

    remove old code

--- WebApplication/server.js
+++ WebApplication/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import mysql from 'mysql2/promise';
 import { CosmosClient } from "@azure/cosmos";
 
 dotenv.config();
@@ -24,48 +23,6 @@ const container = database.container("container1");
 
 // serve static files
 app.use(express.static('public'));
-
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true
-})
-
-app.get('/simulation', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'simulation.html'));
-});
-
-app.get('/api/test-db', async (req, res) => {
-    try {
-        const [rows] = await db.query('SELECT 1 AS connected');
-        res.json({ success: true, result: rows[0] });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: 'Database connection failed' });
-    }
-});
-
-app.get('/api/latest', async (req, res) => {
-    try {
-        const [rows] = await db.query(`
-            SELECT temperature, luminosite, ouverture, mode, status, date
-            FROM data
-            ORDER BY date DESC
-            LIMIT 1
-        `);
-
-        if (rows.length === 0) {
-            return res.json(null);
-        }
-
-        res.json(rows[0]);
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error fetching last data '});
-    }
-});
 
 // API
 app.get("/api/data", async (req, res) => {
